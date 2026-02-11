6e5ddac560ab036ce65bd62f7d77486cdd201cd9
abeamonteiro <abeatrizbmonteiro@gmail.com>
2026-02-11 12:47:43 -0300

    adic componentes navbar, home e footer

--- blogpessoal/src/pages/Home/Home.tsx
+++ blogpessoal/src/pages/Home/Home.tsx
@@ -1,84 +1,30 @@
-import React from 'react'
-
 function Home() {
   return (
       <>
-          <div
-              style={{
-                  backgroundColor: "#312e81",
-                  display: "flex",
-                  justifyContent: "center"
-              }}
-          >
-              <div
-                  style={{
-                      display: "grid",
-                      gridTemplateColumns: "1fr 1fr",
-                      color: "white",
-                      width: "100%",
-                      maxWidth: "1280px",
-                  }}
-              >
-                  <div
-                      style={{
-                          display: "flex",
-                          flexDirection: "column",
-                          gap: "1rem",
-                          alignItems: "center",
-                          paddingTop: "1rem",
-                          paddingBottom: "1rem"
-                      }}
-                  >
-                      <h2
-                          style={{
-                            fontSize: "3rem",
-                            fontWeight: "bold"
-                          }}
-                      >
+          <div className='bg-indigo-900 flex justfy-center'>
+              <div className='container grid grid-cols-2 text-white'>
+                  <div className='flex flex-col gap-4 items-center justify-center py-4'>
+                      <h2>
                           Seja Bem Vinde!
                       </h2>
-
-                      <p
-                          style={{
-                            fontSize: "1.25rem"
-                          }}
-                      >
+                      <p className='text-xl'>
                           Expresse aqui seus pensamentos e opiniões
                       </p>
 
-                      <div
-                          style={{
-                              display: "flex",
-                              justifyContent:"space-around",
-                              gap:"1rem"
-                          }}
-                      >
-                        <div
-                            style={{
-                                borderRadius: "0.5rem",
-                                color:"white",
-                                border:"2px solid white",
-                                padding: "0.5rem 1rem"
-                            }}
-                        >
+                      <div className='flex justify-around gap-4'>
+                        <div className='rounded text-white
+                                        border-white border-solid border-2 py-2 px-4'
+                            >
                             Nova Postagem
-
                         </div>  
                     </div>    
               </div>
 
-              <div
-                  style={{
-                    display: "flex",
-                    justifyContent: "center"
-                  }}
-              >
+              <div className='flex justify-center'>
                   <img
                       src="https://media.licdn.com/dms/image/v2/D4E03AQFz24RHW8mI6g/profile-displayphoto-scale_400_400/B4EZszRx1aKoAg-/0/1766091857946?e=1772064000&v=beta&t=zW9wnG7vcTdEaXfXmmD5truFA3Uga3j8H-j7T_C0Kcs"
                       alt="Imagem Página Home"
-                      style={{
-                          width: "66%"
-                      }}
+                      className='w-2/3'
                   />
              </div>
         </div>
